6b1d9275f11cac636834436dad4631f71befa0c8
melbsurfer <melbsurfer@gmail.com>
2015-07-18 16:43:11 -0400

    React is working using .jsx

---
+++ components/Demo.jsx
@@ -0,0 +1,59 @@
+var Demo = React.createClass({
+	render: function(){
+		return(
+
+			<div className="container">
+				<h1>React Hello World</h1>
+
+				<form>
+					
+					<p className="h3">How did you hear about us?</p>
+
+					<p className="radio">
+						<label>
+							<input type="radio" name="referrer" value="newspaper"/>
+							Newspaper
+						</label>
+					</p>
+					<p className="radio">
+						<label>
+							<input type="radio" name="referrer" value="radio"/>
+							Radio
+						</label>
+					</p>
+					<p className="radio">
+						<label>
+							<input type="radio" name="referrer" value="tv"/>
+							Television
+						</label>
+					</p>
+					<p className="radio">
+						<label>
+							<input type="radio" name="referrer" value="search"/>
+							Search Engine
+						</label>
+					</p>
+					<p className="radio">
+						<label>
+							<input type="radio" name="referrer" value="social"/>
+							Social Media
+						</label>
+					</p>
+					<p className="radio">
+						<label>
+							<input type="radio" name="referrer" value="other"/>
+							Other
+						</label>
+						<label className="form-inline">
+							Please specify:
+							<input type="text" name="referrer_other"/>
+						</label>
+					</p>
+
+					<p><input type="submit"/></p>
+				</form>
+			</div>
+
+		);
+	}
+})
