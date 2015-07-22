c86705f73f11ba4520dc3656129eebf5631230d2
melbsurfer <melbsurfer@gmail.com>
2015-07-22 08:05:33 -0400

    Refactored the 'Other' option

--- components/RadioOptionGroup.jsx
+++ components/RadioOptionGroup.jsx
@@ -1,8 +1,13 @@
 var RadioOptionGroup = React.createClass({
-	
+	onChange: function (event){
+		if (this.props.other){
+			this.refs.other.forceUpdate();
+		}
+		//console.log('test')
+	},
 	render: function(){
 		return(
-			<div>
+			<div onChange={this.onChange}>
 				{this.props.options.map(function(option){
 					return (
 						<RadioOption value={option.value} key={option.value}>
@@ -11,7 +16,7 @@ var RadioOptionGroup = React.createClass({
 					);
 				})}
 
-				{this.props.other && <RadioOtherOption/>}
+				{this.props.other && <RadioOtherOption ref="other" />}
 				
 			</div>
 		);
